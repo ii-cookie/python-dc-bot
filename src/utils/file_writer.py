b1b45ace97ca4cbc94184818a49b6adde411fda0
ii-cookie <isaacnleong@gmail.com>
2025-06-16 16:27:34 +0800

    auto create folder when not created

--- src/utils/file_writer.py
+++ src/utils/file_writer.py
@@ -1,16 +1,13 @@
-#bro i have no idea how to auto make folder so i give up, put the empty folders in urself
-        # currentPath = os.getcwd()
-        # nameofFolder = "extracted_messages"
+from pathlib import Path
 
-        # if not os.path.exists(currentPath + '/private_data' + nameofFolder):
-        #     os.makedirs('/private_data' + nameofFolder)
-
-
-
-
+#---------------------writing extracted msg to txt---------------------------
 def writing_extracted_msg(message, result):
+    nested_directory_path = Path("private_data/Extracted_messages")
+
+    nested_directory_path.mkdir(parents=True, exist_ok=True)
+    print(f"Nested directories '{nested_directory_path}' created successfully.")  
+    
     with open('private_data/extracted_messages/' + message.author.name + '_' + message.channel.name + '_' + str(message.channel.id) + '.txt', 'w') as f:
         for content in result.content:
                 f.write(content + ' \n')
-        # for msg in result.extracted_messages:
-        #         f.write(msg.content + ' \n')
+
