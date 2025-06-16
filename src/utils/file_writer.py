e346408fdaf42658956bfbf38218a958421d8cbe
ii-cookie <isaacnleong@gmail.com>
2025-06-16 17:07:59 +0800

    wrote the utils toggle func (not tested)

--- src/utils/file_writer.py
+++ src/utils/file_writer.py
@@ -11,3 +11,32 @@ def writing_extracted_msg(message, result):
         for content in result.content:
                 f.write(content + ' \n')
 
+
+#---------------------a function that saves and deletes from file on toggles--------------------
+def save_toggle(id, filename):
+    directory_path = Path("private_data")
+    # Create the directory
+    try:
+        directory_path.mkdir()
+        print(f"Directory '{directory_path}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{directory_path}' already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{directory_path}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    id = str(id)
+
+    with open('private_data/' + filename + '.txt', 'w+') as f:
+        all_save_data = f.readlines()
+        if id in all_save_data:
+            for data in all_save_data:
+                if data.strip("\n") != id:
+                    f.write(data)
+            return filename + " is now toggled off"
+        else:
+            for data in all_save_data:
+                f.write(data)
+            f.write(id + '\n')
+            return filename + " is now toggled on"
